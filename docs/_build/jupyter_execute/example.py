009ad771339b0322d776d3cf7763b44c1aea7ac0
ds3478 <90806605+ds3478@users.noreply.github.com>
2021-12-20 12:35:50 -0500

    Sphinx documentation for artsapiclient_ds3478
    Sphinx documentation for artsapiclient_ds3478

---
+++ docs/_build/jupyter_execute/example.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Example usage
+# 
+# To use `artsapiclient_ds3478` in a project:
+
+# In[1]:
+
+
+import artsapiclient_ds3478
+
+print(artsapiclient_ds3478.__version__)
+
